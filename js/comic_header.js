d63b53eaa58d47e918577848368760ce2d8a1464
Whey Isolate <ethankfang@gmail.com>
2026-01-23 02:40:55 -0500

    set up landing page

--- js/comic_header.js
+++ js/comic_header.js
@@ -5,7 +5,7 @@ document.querySelector(".writeHeader").innerHTML = `
         <a href="index.html"><img src="./img/logo.png" alt="" /></a> 
 
         <div id="nav">
-            <a href="index.html">HOME</a> |
+            <a href="comic.html">HOME</a> |
             <a href="archive.html">ARCHIVE</a> |
             <a href="about.html">ABOUT</a> |
             <a href="characters.html">CHARACTERS</a> |
